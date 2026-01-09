3d42843449c84bc99322235e6d69eab232e58585
levine <levine.chin19@gmail.com>
2026-01-09 18:28:45 +0800

    save notes to localStorage

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const addNotesButtons = document.querySelectorAll(".add-notes-btn");
 const closeButton = document.querySelector(".close-btn");
+const cancelButton = document.querySelector(".cancel-btn");
 const dialog = document.getElementById("note-dialog");
+const dialogForm = document.getElementById("dialog-form");
+let notes = [];
 
 addNotesButtons.forEach((button) => {
     button.addEventListener("click", () => {
@@ -17,3 +20,35 @@ dialog.addEventListener("click", (e) => {
 closeButton.addEventListener("click", () => {
     dialog.close();
 });
+
+cancelButton.addEventListener("click", () => {
+    dialog.close();
+    dialogForm.reset();
+});
+
+dialogForm.addEventListener("submit", saveNote);
+
+function saveNote(e) {
+    e.preventDefault();
+
+    const title = document.getElementById("note-title").value.trim();
+    const content = document.getElementById("note-content").value.trim();
+    
+    notes.unshift({
+        id: generateId(),
+        title: title,
+        content: content,
+    });
+
+    saveNotes();
+    dialog.close();
+    dialogForm.reset();
+}
+
+function generateId() {
+    return Date.now().toString();
+}
+
+function saveNotes() {
+    localStorage.setItem('notes', JSON.stringify(notes));
+}
